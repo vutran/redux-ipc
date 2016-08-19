ff1bdc91bd4eb7fdee0c589056593b8f937068be
Vu Tran <vu@vu-tran.com>
2016-08-19 00:29:24 -0700

    added ability to pass an http server

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 const EventEmitter = require('events');
-const WebSocketServer = require('ws').Server;
 
-module.exports = class Server extends EventEmitter {
-  constructor(port, socketName) {
-    super(port, socketName);
-    const self = this;
-    // creates a new server
-    const wss = new WebSocketServer({
-      handleProtocols: (protocols, callback) => {
-        callback(true, socketName);
-      },
-      port,
-    });
-    // listen for connections
-    wss.on('connection', connection => {
-      connection.on('message', message => {
-        self.emit('message', message);
+if (global.process) {
+  const WebSocketServer = require('ws').Server;
+  module.exports = class Server extends EventEmitter {
+    constructor({ server, port, socketName }) {
+      super();
+      const self = this;
+      // creates a new server
+      const wss = new WebSocketServer({
+        server,
+        port,
+        handleProtocols: (protocols, callback) => {
+          callback(true, socketName);
+        },
       });
-    });
-  }
-
-};
+      // listen for connections
+      wss.on('connection', connection => {
+        connection.on('message', message => {
+          self.emit('message', message);
+        });
+      });
+    }
+  };
+} else {
+  module.exports = class Server extends EventEmitter { };
+}
